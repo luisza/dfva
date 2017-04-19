021eea237866d6a65ffaa98bf1345cda54c48c12
Luis Zárate <luis.zarate@solvosoft.com>
2017-04-19 02:24:29 -0600

    fix static path

--- dfva/settings.py
+++ dfva/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT=os.path.join(BASE_DIR, '/static/')
-STATIC_MEDIA=os.path.join(BASE_DIR, '/media/')
+STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
+STATIC_MEDIA=os.path.join(BASE_DIR, 'media/')
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
