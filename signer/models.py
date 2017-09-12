bc3c5edc84f5866dcefde2b9b245f39f1badad9a
Luis Zárate Montero <luis.zarate@solvosoft.com>
2017-09-12 10:49:04 -0600

    OnetoOne relation on request data

--- signer/models.py
+++ signer/models.py
@@ -56,7 +56,7 @@ class SignRequest(models.Model):
 
     arrived_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
-    data_request = models.ForeignKey(
+    data_request = models.OneToOneField(
         SignDataRequest, null=True, blank=True)
     data_hash = models.CharField(max_length=130,
                                  help_text="""Suma hash de datos de tamaño máximo 130 caracteres, usando el
