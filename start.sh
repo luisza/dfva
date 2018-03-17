b7748d8c917eccbef78db7c589043101212d2505
Luis Zárate M <luis.zarate@solvosoft.com>
2018-03-17 14:14:39 -0600

    clean up

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -z $CELERY ]; then
-    bash /dfva_app/start_web.sh
-else
-    bash /dfva_app/start_worker.sh
-fi
--- start_worker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-# Start server
-echo "Starting server"
-export DOCKER=True
-exec celery -A dfva worker -l info -B
-
