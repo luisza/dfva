fb540d57eb048fb8defd67a4af8204371b800345
Luis Zárate M <luis.zarate@solvosoft.com>
2018-11-05 00:43:02 -0600

    install db on run

--- deploy/docker_gunicorn.sh
+++ deploy/docker_gunicorn.sh
@@ -5,6 +5,12 @@ touch /logs/gunicorn.log
 touch /logs/gunicorn-access.log
 tail -n 0 -f /logs/gunicorn*.log &
 
+python manage.py migrate --settings=dfva.settings_docker
+
+if [ ! -f /dfva/internal_ca/ca_key.pem ]; then
+    python manage.py crea_ca --settings=dfva.settings_docker
+fi
+
 export DJANGO_SETTINGS_MODULE=dfva.settings_docker
 
 exec gunicorn dfva.wsgi_docker:application \
