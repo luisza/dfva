6d09d437cbfd1dc0bdcbea15403a5bc622671a5a
Luis Zárate M <luis.zarate@solvosoft.com>
2018-11-04 23:51:57 -0600

    gunicon fixes

--- deploy/docker_gunicorn.sh
+++ deploy/docker_gunicorn.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Prepare log files and start outputting logs to stdout
-touch ./logs/gunicorn.log
-touch ./logs/gunicorn-access.log
-tail -n 0 -f ./logs/gunicorn*.log &
+touch /logs/gunicorn.log
+touch /logs/gunicorn-access.log
+tail -n 0 -f /logs/gunicorn*.log &
 
-export DJANGO_SETTINGS_MODULE=projectx.settings
+export DJANGO_SETTINGS_MODULE=dfva.settings_docker
 
 exec gunicorn dfva.wsgi_docker:application \
     --name dfva \
     --bind 0.0.0.0:8000 \
     --workers 5 \
     --log-level=info \
-    --log-file=./logs/gunicorn.log \
-    --access-logfile=./logs/gunicorn-access.log \
+    --log-file=/logs/gunicorn.log \
+    --access-logfile=/logs/gunicorn-access.log \
 "$@"
