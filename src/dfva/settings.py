70a30743eec94bba14dd3c4c160e863e58a92468
Luis Zárate M <luis.zarate@solvosoft.com>
2020-06-11 00:16:53 -0600

    Set in english for easy debuging

--- src/dfva/settings.py
+++ src/dfva/settings.py
@@ -149,7 +149,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'America/Costa_Rica'
 
